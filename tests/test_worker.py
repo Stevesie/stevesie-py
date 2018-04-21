99e0b6d567bac533ec9f5863fa1f8ea9b33430cc
Steve Spagnola <steve@stevesie.com>
2018-04-21 16:43:15 -0400

    try testing

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -8,3 +8,9 @@ def test_init():
     worker = Worker(worker_id)
     assert worker.id == worker_id
     assert worker.is_hydrated == False
+
+def test_hydration():
+    worker_id = uuid.uuid4()
+    worker = Worker(worker_id)
+    worker.fetch()
+    assert worker.is_hydrated == True
